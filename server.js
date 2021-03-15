c9c27d3d7415b2fb6df3290e3e10e7e4bbad4f23
Molly Carroll <mollyajcarroll@gmail.com>
2021-03-15 12:49:00 -0500

    add cors dependency and cors options

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = 3003;
 
 const dbname = 'events';
+
+// middleware
+app.use(express.json());
 
 // error / disconnection
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongo not running?'));
@@ -16,6 +20,20 @@ mongoose.connect(`mongodb://localhost:27017/${dbname}`);
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose! ~~~')
 });
+
+// whitelist and cors
+const whitelist = ['http://localhost: 3000'];
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('not allowed by CORS'));
+        }
+    }
+}
+
+app.use(cors(corsOptions));
 
 // controller
 const eventsController = require('./controllers/event_controller.js');
